6cb5fd9a68af51df43fc68d2c0d37668b29c7b2e
maxwshen <maxwshen@gmail.com>
2021-07-25 17:57:00 -0400

    first

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, redirect, render_template
+
+app = Flask(__name__)
+
+
+#what to serve for a given page.
+@app.route('/')
+def index():
+# def index(page):
+  # return render_template('/articles/' + page + '.html')
+  return 'Hello world!'
+
+app.run(debug=True)
